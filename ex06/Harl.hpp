63ba0571760274532bcc590a84e5a97fef7c16f6
Mahad Abdullah <maabdull@student.42abudhabi.ae>
2025-02-15 13:42:44 +0400

    chore: add ex06

---
+++ ex06/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maabdull <maabdull@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 11:10:17 by maabdull          #+#    #+#             */
+/*   Updated: 2025/02/15 12:30:45 by maabdull         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class	Harl {
+	public:
+		void	complain( std::string level );
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+};
+
+#endif
---
+++ ex06/main.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maabdull <maabdull@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 17:04:36 by maabdull          #+#    #+#             */
+/*   Updated: 2025/02/15 13:41:29 by maabdull         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+enum	LOG_LEVEL {
+	DEBUG,
+	INFO,
+	WARNING,
+	ERROR,
+	INVALID,
+};
+
+LOG_LEVEL	getLogLevel(std::string levelStr) {
+	if (levelStr == "DEBUG")
+		return DEBUG;
+	if (levelStr == "INFO")
+		return INFO;
+	if (levelStr == "WARNING")
+		return WARNING;
+	if (levelStr == "ERROR")
+		return ERROR;
+	return INVALID;
+}
+
+int	main(int argc, char *argv[]) {
+	if (argc != 2) {
+		cout << "[ Probably complaining about insignificant problems ]" << endl;
+		return (0);
+	}
+	LOG_LEVEL logLevel = getLogLevel(std::string(argv[1]));
+	Harl	harl;
+	switch (logLevel)
+	{
+		default:
+			cout << "[ Probably complaining about insignificant problems ]" << endl;
+			break;
+		case DEBUG:
+		 	cout << "[ DEBUG ]\n";
+			harl.complain("DEBUG");
+			cout << endl;
+		case INFO:
+		 	cout << "[ INFO ]\n";
+			harl.complain("INFO");
+			cout << endl;
+		case WARNING:
+		 	cout << "[ WARNING ]\n";
+			harl.complain("WARNING");
+			cout << endl;
+		case ERROR:
+		 	cout << "[ ERROR ]\n";
+			harl.complain("ERROR");
+			cout << endl;
+	}
+	return (0);
+}
