8b4805f6434c3d0203cdad87eaade4233cc738da
mahahahad <mahad.abd2003@gmail.com>
2025-02-23 14:44:04 +0400

    style: indent with spaces

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -7,14 +7,14 @@
 using std::cout;
 using std::endl;
 
-class	Harl {
-	public:
-		void	complain( std::string level );
-	private:
-		void	debug( void );
-		void	info( void );
-		void	warning( void );
-		void	error( void );
+class   Harl {
+    public:
+        void    complain( std::string level );
+    private:
+        void    debug( void );
+        void    info( void );
+        void    warning( void );
+        void    error( void );
 };
 
 #endif
